ea3bc8522f3aa54c5c67f7e81e8a68f2821b325a
Vinayak Yande <vcharms@gmail.com>
2022-10-06 14:50:44 +0530

    added pymemcache and CACHES in the settings.py

--- gitdemoproject/settings.py
+++ gitdemoproject/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
